f601325245a01c3ef2109cd13ca147f2520427a7
rx1310 <rx1310@inbox.ru>
2020-06-13 02:21:43 +0300

    Запуск репозитория

---
+++ app/src/main/java/o1310/rx1310/app/cdpi/MainActivity.java
@@ -0,0 +1,22 @@
+/*
+ * @author      rx1310 <rx1310@inbox.ru>
+ * @copyright   Copyright (c) o1310, 2020
+ * @license     MIT License
+ */
+
+package o1310.rx1310.app.cdpi;
+
+import android.app.*;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_main);
+		
+	}
+	
+}
