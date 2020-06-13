677038b058f37f7f52ec87e182829d8912039996
rx1310 <rx1310@inbox.ru>
2020-06-13 14:16:39 +0300

    Поправил интерфейс

--- app/src/main/java/o1310/rx1310/app/cdpi/MainActivity.java
+++ app/src/main/java/o1310/rx1310/app/cdpi/MainActivity.java
@@ -7,16 +7,21 @@
 package o1310.rx1310.app.cdpi;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	Button applyButton;
 	EditText inputField;
+	TextView appInfoText;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,15 @@ public class MainActivity extends Activity {
 		
 		inputField = findViewById(R.id.etInputNum);
 		
+		appInfoText = findViewById(R.id.tvAppInfo);
+		appInfoText.setText(String.format(getString(R.string.desc_about), appVersion(this)));
+		appInfoText.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "made with " + ("♥️") + " by rx1310 (from o1310)", Toast.LENGTH_LONG).show();
+			}
+		});
+		
 		applyButton = findViewById(R.id.btnApply);
 		applyButton.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -36,6 +50,27 @@ public class MainActivity extends Activity {
 			}
 		});
 		
+	}
+	
+	public static String appVersion(Context c) {
+
+		String s, a;
+		int v;
+		
+		PackageManager m = c.getPackageManager();
+
+		try {
+			PackageInfo i = m.getPackageInfo(c.getPackageName(), 0);
+			s = i.versionName;
+			v = i.versionCode;
+			a = s + ", сборка " + v;
+		} catch(PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+			a = "error(String.appVersion)";
+		}
+
+		return a;
+
 	}
 	
 }
