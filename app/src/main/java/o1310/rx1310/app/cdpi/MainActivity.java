f17f6c40fdb54912d997e606b4b3696a4776208d
rx1310 <rx1310@inbox.ru>
2020-06-13 03:05:50 +0300

    Немного доработал код

--- app/src/main/java/o1310/rx1310/app/cdpi/MainActivity.java
+++ app/src/main/java/o1310/rx1310/app/cdpi/MainActivity.java
@@ -6,17 +6,34 @@
 
 package o1310.rx1310.app.cdpi;
 
-import android.app.*;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	Button applyButton;
+	EditText inputEditText;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setTitle(R.string.activity_main);
 		setContentView(R.layout.activity_main);
+		
+		inputEditText = findViewById(R.id.etInputNum);
+		
+		applyButton = findViewById(R.id.btnApply);
+		applyButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
 		
 	}
 	
